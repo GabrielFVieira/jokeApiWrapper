4c49303da6a6cdc266d74e5536e96807fdbcdc88
Gabriel Figueiredo <gabrielfigueiredov@gmail.com>
2021-10-31 12:40:11 -0300

    Generated base project
    Project generated using the spring initilizr with the Spring Web, Spring Data JPA and MySQLDriver dependencies.

---
+++ src/test/java/com/gabrielfigueiredo/jokeApiWrapper/JokeApiWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielfigueiredo.jokeApiWrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeApiWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
